a2973083b469d392d758666ac1076200f491553c
1278354705 <1278354705@qq.com>
2018-08-17 15:44:59 +0800

    插入数据

---
+++ src/test/resources/db.sql
@@ -0,0 +1,20 @@
+--删表
+ drop table Sale;
+ drop table Type;
+ drop table series;
+ drop table product;
+ drop table admin;
+ drop table operator;
+ drop table customer;
+ drop table bill;
+ drop table detail;
+ 
+ --查询
+ select * from Sale;
+ 
+ select * from Type;
+ 
+ select * from series;
+ 
+ --删数据
+ delete from Type where 1=1;
---
+++ src/test/resources/init.sql
@@ -0,0 +1,46 @@
+--
+insert into Sale(Sname,Sstatus) values('笔记本电脑',1);
+insert into Sale(Sname,Sstatus) values('整机产品',1);
+insert into Sale(Sname,Sstatus) values('主机/显卡/显示器',1);
+
+--类型
+insert into Type(Sid,Tname,Tstatus) values(1,'灵耀轻薄本',1);
+insert into Type(Sid,Tname,Tstatus) values(1,'ROG',1);
+insert into Type(Sid,Tname,Tstatus) values(1,'游戏笔记本',1);
+insert into Type(Sid,Tname,Tstatus) values(2,'台式机',1);
+insert into Type(Sid,Tname,Tstatus) values(2,'一体机',1);
+insert into Type(Sid,Tname,Tstatus) values(2,'Mini PC',1);
+
+--系列
+insert into series(Tid,Sename,Sestatus) values(1,'灵耀3系列',1);
+insert into series(Tid,Sename,Sestatus) values(1,'灵耀U系列',1);
+insert into series(Tid,Sename,Sestatus) values(1,'灵耀S系列',1);
+
+insert into series(Tid,Sename,Sestatus) values(2,'ROG系列',1);
+
+insert into series(Tid,Sename,Sestatus) values(3,'顽石-FL/YX系列',1);
+insert into series(Tid,Sename,Sestatus) values(3,'飞行堡垒-FX/ZX系列',1);
+
+insert into series(Tid,Sename,Sestatus) values(3,'飞行堡垒-FX/ZX系列',1);
+
+
+
+
+--产品
+insert into product(Seid,Pname,Pdetails,Pparameter,Pprice,Pcount,Pstatus) values(1,'Zenbook 3U-灵耀3系列轻薄笔记本（Windows 10 Home/i7-7500U/8GB/256GB SSD）-尊爵蓝',
+'品牌_ASUS:所属分类_灵耀3系列:操作系统_Windows 10 Home:键盘背光_是:CPU型号_i7-7500U','主要信息:商品型号_Zenbook 3U/操作系统_Windows 10 Home+Office 2016;处理器:CPU类型_搭载第七代Intel® Kabylake CPU/CPU型号_Intel® Kabylake CPU i7-7500U',
+10149.22,300,1);
+insert into product(Seid,Pname,Pdetails,Pparameter,Pprice,Pcount,Pstatus) values(2,'Zenbook 3U-灵耀3系列轻薄笔记本（Windows 10 Home/i7-7500U/8GB/256GB SSD）-尊爵蓝',
+'品牌_ASUS:所属分类_灵耀3系列:操作系统_Windows 10 Home:键盘背光_是:CPU型号_i7-7500U','主要信息:商品型号_Zenbook 3U/操作系统_Windows 10 Home+Office 2016;处理器:CPU类型_搭载第七代Intel® Kabylake CPU/CPU型号_Intel® Kabylake CPU i7-7500U',
+10149.22,300,1);
+insert into product(Seid,Pname,Pdetails,Pparameter,Pprice,Pcount,Pstatus) values(3,'Zenbook 3U-灵耀3系列轻薄笔记本（Windows 10 Home/i7-7500U/8GB/256GB SSD）-尊爵蓝',
+'品牌_ASUS:所属分类_灵耀3系列:操作系统_Windows 10 Home:键盘背光_是:CPU型号_i7-7500U','主要信息:商品型号_Zenbook 3U/操作系统_Windows 10 Home+Office 2016;处理器:CPU类型_搭载第七代Intel® Kabylake CPU/CPU型号_Intel® Kabylake CPU i7-7500U',
+10149.22,300,1);
+
+
+
+
+
+
+
+
